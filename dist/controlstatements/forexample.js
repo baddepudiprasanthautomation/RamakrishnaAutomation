3164a7c217a6ca0107d6edcf5fe961343b84073a
Prasanth Baddepudi <baddepudiprasanthautomation@gmail.com>
2026-07-09 08:47:40 +0530

    for example

---
+++ dist/controlstatements/forexample.js
@@ -0,0 +1,15 @@
+"use strict";
+//initialization; condition check;increment/decrement 
+for (let i = 0; i <= 100; i++) {
+    console.log(i);
+    i += 1;
+}
+// i = 0 
+// condition 0<=100 -> true -> allow into loop -> print number 
+// increment -> i becomes i = 1 then check condition
+// condition 1<=100 -> true -> allow into loop -> print number 
+// increment -> i becomes i = 2 then check condition
+// condition 1<=100 -> true -> allow into loop -> print number
+//finally // increment -> i becomes i = 101 then check condition
+//condition 101<=100 -> false -> don't allow into loop -> exit
+//# sourceMappingURL=forexample.js.map
