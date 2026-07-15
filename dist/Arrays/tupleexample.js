8665543d8c48b926253859629169eb8c73f46cec
Prasanth Baddepudi <prasanthbaddepudiautomation@gmail.com>
2026-07-15 08:53:45 +0530

    tuple examples

---
+++ dist/Arrays/tupleexample.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// tuple is a type of data type used to store data in different types of data in same variable
+/*
+    syntax: let variableName:[number,string,number] = [101,"Prasanth",20000]
+*/
+let employeeDetails = [101, "Prasanth", 20000];
+console.log(employeeDetails);
+// tuples with rest elements
+let marks = ["prasanth", 98, 45, 67, 65, 34, 77];
+console.log(marks);
+//updating tuple
+employeeDetails[1] = "Ramakrishna";
+console.log(employeeDetails);
+//# sourceMappingURL=tupleexample.js.map
